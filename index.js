b6f56fb0c9962349fc2322bfbfb7987a7802ca86
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2021-03-30 11:22:17 +0200

    to heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const vg = require('vega');
+//const express = require('express');
+//const vg = require('vega');
 
 const app = express();
 
@@ -14,17 +14,18 @@ const TYPES = {
   ZurichMap: require('./charts/ZurichMap')
 };
 
+/*
 
 app.get('/:type', (request, response) => {
-    /*
-  if (!TYPES[request.params.type]) {
-    return response.status(404).send('Not Found');
-  }
 
-  const spec = TYPES[request.params.type]({
-    spec: JSON.parse(request.query.spec)
-  });
-  */
+//   if (!TYPES[request.params.type]) {
+//     return response.status(404).send('Not Found');
+//   }
+
+//   const spec = TYPES[request.params.type]({
+//     spec: JSON.parse(request.query.spec)
+//   });
+  
 
   var spec = JSON.parse(request.query.spec);
 
@@ -47,126 +48,11 @@ app.get('/:type', (request, response) => {
         .catch(function(err) { console.error(err); });
 
 });
-
+*/
 
 app.get('/', (req, response) => {
     
     response.send('Hello world');
-    return;
-    
-    var spec = {
-        "$schema": "https://vega.github.io/schema/vega/v5.json",
-        "description": "A basic bar chart example, with value labels shown upon mouse hover.",
-        "width": 400,
-        "height": 200,
-        "padding": 5,
-    
-        "data": [
-        {
-            "name": "table",
-            "values": [
-            {"category": "A", "amount": 28},
-            {"category": "B", "amount": 55},
-            {"category": "C", "amount": 43},
-            {"category": "D", "amount": 91},
-            {"category": "E", "amount": 81},
-            {"category": "F", "amount": 53},
-            {"category": "G", "amount": 19},
-            {"category": "H", "amount": 87}
-            ]
-        }
-        ],
-    
-        "signals": [
-        {
-            "name": "tooltip",
-            "value": {},
-            "on": [
-            {"events": "rect:mouseover", "update": "datum"},
-            {"events": "rect:mouseout",  "update": "{}"}
-            ]
-        }
-        ],
-    
-        "scales": [
-        {
-            "name": "xscale",
-            "type": "band",
-            "domain": {"data": "table", "field": "category"},
-            "range": "width",
-            "padding": 0.05,
-            "round": true
-        },
-        {
-            "name": "yscale",
-            "domain": {"data": "table", "field": "amount"},
-            "nice": true,
-            "range": "height"
-        }
-        ],
-    
-        "axes": [
-        { "orient": "bottom", "scale": "xscale" },
-        { "orient": "left", "scale": "yscale" }
-        ],
-    
-        "marks": [
-        {
-            "type": "rect",
-            "from": {"data":"table"},
-            "encode": {
-            "enter": {
-                "x": {"scale": "xscale", "field": "category"},
-                "width": {"scale": "xscale", "band": 1},
-                "y": {"scale": "yscale", "field": "amount"},
-                "y2": {"scale": "yscale", "value": 0}
-            },
-            "update": {
-                "fill": {"value": "steelblue"}
-            },
-            "hover": {
-                "fill": {"value": "red"}
-            }
-            }
-        },
-        {
-            "type": "text",
-            "encode": {
-            "enter": {
-                "align": {"value": "center"},
-                "baseline": {"value": "bottom"},
-                "fill": {"value": "#333"}
-            },
-            "update": {
-                "x": {"scale": "xscale", "signal": "tooltip.category", "band": 0.5},
-                "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -2},
-                "text": {"signal": "tooltip.amount"},
-                "fillOpacity": [
-                {"test": "datum === tooltip", "value": 0},
-                {"value": 1}
-                ]
-            }
-            }
-        }
-        ]
-    };
-
-    var view = new vg.View(
-        vg.parse(spec),
-        {
-            renderer: 'svg'
-        }
-    );
- 
-    // generate a static SVG image
-    view.toSVG()
-        .then(function(svg) {
-        
-            response
-            .set('Cache-Control', `public, max-age=${60 * 60}`)
-                .type('svg').send(svg);
-        })
-        .catch(function(err) { console.error(err); });
 });
 
 const PORT = process.env.PORT || 3000
